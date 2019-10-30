3ea5c89b9ab82699cd785e708c672f4697c69658
Justin Pak <jpak1203@gmail.com>
2019-10-29 22:20:03 -0400

    added project arrow functionality

--- scripts/projects.js
+++ scripts/projects.js
@@ -177,3 +177,90 @@ function noSelector() {
     navSelection.style.opacity = 0;
     navSelection.textContent = "";
 }
+
+
+// PROJECT SPINNER
+
+const projectReel = document.getElementsByClassName("project-reel");
+const projectSelection = document.querySelectorAll("#proj-selection div");
+let index = -1;
+
+const next = document.querySelector("#next");
+const prev = document.querySelector("#prev");
+
+next.addEventListener("click", (e) => {
+    if (index == -1) {
+        document.querySelector("#proj-home").style.opacity = 0;
+        document.querySelector("#prev").style.opacity = 1;
+    }
+    index++;
+    if (index > projectReel.length-1) {
+        projectSelection[index-1].style.opacity = 0.2;
+        projectReel[index-1].style.opacity = 0;
+        projectReel[index-1].style.pointerEvents = "none";
+        index = 0;
+    }
+    if (index > 0) {
+        projectSelection[index-1].style.opacity = 0.2;
+        projectReel[index-1].style.opacity = 0;
+        projectReel[index-1].style.pointerEvents = "none";
+    }
+    projectSelection[index].style.opacity = 1;
+    projectReel[index].style.opacity = 1;
+    projectReel[index].style.pointerEvents = "auto";
+});
+
+prev.addEventListener("click", (e) => {
+    index--;
+    if (index < 0) {
+        projectSelection[index+1].style.opacity = 0.2;
+        projectReel[index+1].style.opacity = 0;
+        projectReel[index+1].style.pointerEvents = "none";
+        index = projectReel.length-1;
+    }
+    if (index < projectReel.length-1) {
+        projectSelection[index+1].style.opacity = 0.2;
+        projectReel[index+1].style.opacity = 0;
+        projectReel[index+1].style.pointerEvents = "none";
+    }
+    projectSelection[index].style.opacity = 1;
+    projectReel[index].style.opacity = 1;
+    projectReel[index].style.pointerEvents = "auto";
+});
+
+for (var p = 0; p < projectSelection.length; p++) {
+    projectSelection[p].addEventListener("click", selectProject);
+}
+
+function selectProject(){
+    if (index !== -1) {
+        projectSelection[index].style.opacity = 0.2;
+        projectReel[index].style.opacity = 0;
+        projectReel[index].style.pointerEvents = "none";
+    } else {
+        document.querySelector("#proj-home").style.opacity = 0;
+        document.querySelector("#prev").style.opacity = 1;
+        
+    }
+
+    switch (this.id) {
+        case "proj1":
+            index = 0;
+
+            projectSelection[index].style.opacity = 1;
+            projectReel[index].style.opacity = 1;
+            projectReel[index].style.pointerEvents = "auto";
+            break;
+        case "proj2":
+            index = 1;
+
+            projectSelection[index].style.opacity = 1;
+            projectReel[index].style.opacity = 1;
+            projectReel[index].style.pointerEvents = "auto";
+            break;
+        case "proj3":
+            break;
+        case "proj4":
+            break;
+    }
+}
